56eebd7c062471afad40f90d1dc137de48bfbb03
Egezenn <88906665+Egezenn@users.noreply.github.com>
2022-06-20 17:49:44 +0000

    Add files via upload

---
+++ 100dofcode/01/01 - nothing of importance.py
@@ -0,0 +1,34 @@
+# just asks the fellow brederen what age they are
+# print("you're "+ input("age my brederen?\nAnswer:")+"? Noice")
+
+# calculates the length of any name you enter
+# print(len(input("NAME PLS\n")))
+
+# randomtext=input("xoxo\n")
+# print(len(randomtext))
+
+# a = "dinnng"
+# print(a)
+# print("length of that is")
+# print(len(a))
+
+# b = "doinnng"
+# print(b)
+# print("and the length of that is")
+# print(len(b))
+
+# a = input("a:")
+# b = input("b:")
+
+# c = a
+# a = b
+# b = c
+
+# print("a = "+a)
+# print("b = "+b)
+
+
+# print("welcome to word thingy")
+# word1 = input("type A word you\n")
+# word2 = input("type ANOTHER word\n")
+# print(word1+" "+word2)
