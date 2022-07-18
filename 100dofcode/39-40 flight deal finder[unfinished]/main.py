812bf56562f5dd2e417c25b87ef7a663a814c5c0
Egezenn <88906665+Egezenn@users.noreply.github.com>
2022-07-18 10:41:04 +0000

    Create main.py

---
+++ 100dofcode/39-40 flight deal finder[unfinished]/main.py
@@ -0,0 +1,4 @@
+# I honestly don't want to mess with requests&apis for another week.
+# Also coding 4 classes & main for 2 days sort of bugged me out.
+# I want to learn new things not master requests&apis right now.
+# I'll come back later.
